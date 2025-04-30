9a665abea76726abd09509d8efb2d3cd76719bf7
Daniel Vasquez <dwvasquez97@gmail.com>
2025-04-29 21:50:45 -0700

    added better layout and started to add text to the pages

--- frontend/src/app/about/page.js
+++ frontend/src/app/about/page.js
@@ -1,8 +1,52 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import Image from "next/image";
+import "./about.css";
 
 export default function About() {
   return (
-    <div>About Page</div>
+    <>
+      <h1>About Page</h1>
+      <h2>Meet Our Team</h2>
+      <div>
+        <Image
+          src="globe.svg"
+          width={300}
+          height={300}
+          alt="Picture of Nicole Thompson-Stockmoe"
+        />
+        <h3>Nicole Thompson-Stockmoe</h3>
+        <h4>Mortgage Broker - Loan Officer - Contract Processor</h4>
+        <p>
+          With over two decades in the mortgage industry, I bring extensive
+          knowledge and a proven track record to every transaction. My
+          experience spans from processing and loan origination to being the
+          Broker/Owner of S2 Mortgage, providing me with a comprehensive
+          understanding of the industry. My commitment to excellence ensures
+          that clients receive top-tier service and support throughout their
+          mortgage journey. Dedicated to client satisfaction, I offer trusted
+          guidance and personalized solutions for all your mortgage needs,
+          whether you're purchasing your first home, refinancing, or exploring
+          investment opportunities.
+        </p>
+        <h5>DRE License#: 01872714</h5>
+        <h5>NMLS#: 322857</h5>
+      </div>
+
+      <div>
+        <Image
+          src="window.svg"
+          width={300}
+          height={300}
+          alt="Picture of Glenn Siaumau"
+        />
+        <h3>Glenn Siaumau</h3>
+        <h4>Loan Officer</h4>
+        <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        </p>
+        <h5>DRE License#: 01415271</h5>
+        <h5>NMLS#: 2278633</h5>
+      </div>
+    </>
   );
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,8 +5,91 @@ import "./globals.css";
 // src/app/page.js
 export default function Home() {
   return (
-  <div className="homepage">
-    <button><a href="https://plus.preapp1003.com/Nicole-Thompson-Stockmoe">Get Started Now</a></button>
-  </div>
+    <>
+      <div className="homepage">
+        <button>
+          <a href="https://plus.preapp1003.com/Nicole-Thompson-Stockmoe">
+            Get Started Now
+          </a>
+        </button>
+      </div>
+
+      <div>
+        <h2>
+          At S2 Mortgage, we take pride in delivering exceptional service and
+          are confident that we have the perfect solution to you unique home
+          loan needs.
+        </h2>
+      </div>
+
+      <div>
+        <h2>Comprehensive Mortgage Consultation Services</h2>
+        <p>
+          At S2 Mortgage, we take pride in the service we provide and are
+          confident that we have the solution for your individual home loan
+          needs.
+        </p>
+      </div>
+
+      <div>
+        <h2>What We Offer</h2>
+        <ul>
+          <li>Home Mortgage Loans</li>
+          {/* We offer various options tailored to your unique needs. Our goal is to find the best mortgage solution for your financial situation. */}
+          <li>Prequalification Services</li>
+          {/* Our detailed prequalification services will give you insight into your home buying power. Understanding your options helps streamline the home buying process and gives you a competitice edge. */}
+          <li>Contract Loan Processing</li>
+          {/* Our experienced team provides efficient and reliable contract loan processing services, ensuring loan originators can focus on originating without the expense of having a processor on payroll. */}
+          <li>Servicing Those Who Serve</li>
+          {/* Supporting those who have served our country. Through providing VA loans to our U.S. Veterans and Surviving Spouses, we are proud to deliver the exceptional service you deserve. */}
+        </ul>
+      </div>
+
+      <div>
+        <h2>Why Choose Us</h2>
+        <div>
+          <h3>Commitment to Excellence</h3>
+          <p>
+            We prioritize your needs and focus on providing a seamless and
+            stress-free mortgage experience. Our ethical approach ensures you
+            receive honest advice and transparent service.
+          </p>
+        </div>
+        <div>
+          <h3>Local Expertise</h3>
+          <p>
+            Serving San Diego, Los Angeles, and surrounding areas, we understand
+            the local market and are well-equipped to assist you with your
+            mortgage needs in California.
+          </p>
+        </div>
+        <div>
+          <h3>Client-Centric Approach</h3>
+          <p>
+            Our commitment to exceptional service means we put your best
+            interests first. We’re here to guide you through every step and
+            ensure you feel confident and informed.
+          </p>
+        </div>
+        <div>
+          <h3>Reliable Support</h3>
+          <p>
+            Whether you are a First Time Homebuyer or a seasoned real estate
+            investor, our dedicated team is ready to provide reliable support
+            and answer your questions.
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <h3>Explore Our Services</h3>
+        <p>
+          Start your journey with S2 Mortgage, and let us help you achieve your
+          home financing goals. Discover our full range of services and find the
+          one that aligns with your needs.
+        </p>
+        <button>Explore Our Services</button>
+      </div>
+    </>
   );
 }
