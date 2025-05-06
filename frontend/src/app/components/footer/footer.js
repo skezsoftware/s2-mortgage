6f394369170a6cf839811f08e8d9197816fe6cbd
Daniel Vasquez <dwvasquez97@gmail.com>
2025-05-05 22:21:34 -0700

    added new images along with messing around with styling and color

---
+++ frontend/src/app/components/footer/footer.js
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <footer style={{ padding: "1rem" }} className="footer">
+      <div>
+        <p>&copy; 2023 S2 Mortgage. All rights reserved.</p>
+      </div>
+      <div>
+        <p>Privacy Policy | Terms of Service</p>
+      </div>
+    </footer>
+  );
+}
--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
