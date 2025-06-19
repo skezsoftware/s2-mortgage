84913bf5225025694b99f545023ab186db03d2c0
Daniel Vasquez <dwvasquez97@gmail.com>
2025-06-19 13:49:13 -0700

    adjusted the loan process page

--- frontend/src/app/loan_process/page.js
+++ frontend/src/app/loan_process/page.js
@@ -21,15 +21,24 @@ export default function LoanProcess() {
       </section>
 
       {/* Loan Process Steps */}
-      <div className="stagesCard" style={{ textAlign: "left", margin: "0 auto", maxWidth: "1100px", marginBottom: "2.5rem", marginTop: "2.5rem"}}>
+      <div
+        className="stagesCard"
+        style={{
+          textAlign: "left",
+          margin: "0 auto",
+          maxWidth: "1100px",
+          marginBottom: "2.5rem",
+          marginTop: "2.5rem",
+        }}
+      >
         <h1>The Stages of a Home Loan</h1>
         <h2>For Home Buyers</h2>
         <p>
           Navigating the home loan process can seem daunting, but understanding
-          each stage can make it much smoother. At S2 Mortgage, we&apos;re here to
-          guide you through every step, ensuring you have the support you need
-          to secure your dream home. Here&apos;s a breakdown of the key stages in the
-          home loan process:
+          each stage can make it much smoother. At S2 Mortgage, we&apos;re here
+          to guide you through every step, ensuring you have the support you
+          need to secure your dream home. Here&apos;s a breakdown of the key
+          stages in the home loan process:
         </p>
       </div>
 
@@ -39,9 +48,16 @@ export default function LoanProcess() {
           <div className="stageContent">
             <h2>Pre-Approval</h2>
             <h3>WHAT IT IS:</h3>
-            <p>Once you&apos;ve picked a home, you&apos;ll need to fill out a loan application with details about your income, assets, and debts.</p>
+            <p>
+              Once you decide to buy a new home, before you start looking at
+              properties, you&apos;ll need to fill out a loan application with
+              details about your income, assets, and debts.
+            </p>
             <h3>WHY IT MATTERS:</h3>
-            <p>Getting pre-approved shows you your budget and strengthens your offer on a home.</p>
+            <p>
+              Getting pre-approved shows you your budget and strengthens your
+              offer on a home, helping to prevent any surprises later on.
+            </p>
           </div>
         </div>
         <div className="stagesCard">
@@ -49,9 +65,15 @@ export default function LoanProcess() {
           <div className="stageContent">
             <h2>Find a Home</h2>
             <h3>WHAT IT IS:</h3>
-            <p>Once you&apos;ve picked a home, you&apos;ll need to fill out a loan application with details about your income, assets, and debts.</p>
+            <p>
+              Provide your agent with your Pre-Approval letter from S2 Mortgage
+              to elevate your offer.
+            </p>
             <h3>WHY IT MATTERS:</h3>
-            <p>Choosing the right home sets the stage for your mortgage process and helps us tailor your loan options to match your goals.</p>
+            <p>
+              Choosing the right home sets the stage for your mortgage process
+              and helps us tailor your loan options to match your goals.
+            </p>
           </div>
         </div>
         <div className="stagesCard">
@@ -59,9 +81,16 @@ export default function LoanProcess() {
           <div className="stageContent">
             <h2>Underwriting</h2>
             <h3>WHAT IT IS:</h3>
-            <p>During underwriting, the lender will review your application, financial documents, and property details to confirm your loan eligibility.</p>
+            <p>
+              During underwriting, the lender will review your application,
+              financial documents, and property details to confirm your loan
+              eligibility.
+            </p>
             <h3>WHY IT MATTERS:</h3>
-            <p>This stage ensures the information provided is accurate & meets the lending criteria, helping to prevent any surprises later on.</p>
+            <p>
+              This stage ensures the information provided is accurate & meets
+              the lending criteria.
+            </p>
           </div>
         </div>
         <div className="stagesCard">
@@ -69,9 +98,16 @@ export default function LoanProcess() {
           <div className="stageContent">
             <h2>Loan Approval</h2>
             <h3>WHAT IT IS:</h3>
-            <p>After underwriting, you&apos;ll get formal loan approval with the terms and conditions. We&apos;ll then finalize the details and prepare for closing.</p>
+            <p>
+              After underwriting, you&apos;ll get formal loan approval with the
+              terms and conditions. We&apos;ll then finalize the details and
+              prepare for closing.
+            </p>
             <h3>WHY IT MATTERS:</h3>
-            <p>Loan approval confirms that you are set to proceed with the purchase of your home, pending any final conditions.</p>
+            <p>
+              Loan approval confirms that you are set to proceed with the
+              purchase of your home, pending any final conditions.
+            </p>
           </div>
         </div>
         <div className="stagesCard">
@@ -79,9 +115,16 @@ export default function LoanProcess() {
           <div className="stageContent">
             <h2>Closing</h2>
             <h3>WHAT IT IS:</h3>
-            <p>Closing is the final step where you sign the documents, pay any remaining fees, and officially buy the home.</p>
+            <p>
+              Closing is the final stage where you sign your loan documents and
+              send any additional down payment and closing funds to escrow.
+            </p>
             <h3>WHY IT MATTERS:</h3>
-            <p>Successful closing means you&apos;re officially a homeowner! It&apos;s the culmination of all the hard work and preparation you&apos;ve put in.</p>
+            <p>
+              Successful closing means you&apos;re officially a homeowner!
+              It&apos;s the culmination of all the hard work and preparation
+              you&apos;ve put in.
+            </p>
           </div>
         </div>
         <div className="stagesCard emptyCard"></div>
