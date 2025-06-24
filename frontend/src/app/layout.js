0e64f596ebf9f28004d0539736b5b96b95fd52b2
Daniel Vasquez <dwvasquez97@gmail.com>
2025-06-24 16:54:27 -0700

    adjusted phone view and colors

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,8 +1,8 @@
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-import UpButton from './components/up_button/upButton';
+import UpButton from "./components/up_button/upButton";
 import "./globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const metadata = {
   title: "S2 Mortgage",
@@ -12,6 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon_2.png" type="image/png" />
+      </head>
       <body>
         <Header />
         {children}
